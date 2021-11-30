d6b9a56e55b1226c00cc5dbf9f0381e3e57d2944
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2021-11-30 21:30:27 +0300

    heroku settings

---
+++ devnote-client/server.js
@@ -0,0 +1,8 @@
+﻿const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/devnote-client'));
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname+
+        '/dist/<app-name>/index.html'));});
+app.listen(process.env.PORT || 8080);
