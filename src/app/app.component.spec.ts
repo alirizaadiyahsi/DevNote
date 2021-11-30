17d577cada6d7ca1edccd78a06d430712d65c718
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2021-11-30 22:43:47 +0300

    minor changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'devnote-client'`, () => {
+  it(`should have as title 'devnote'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('devnote-client');
+    expect(app.title).toEqual('devnote');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('devnote-client app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('devnote app is running!');
   });
 });
